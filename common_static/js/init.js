bfb5bbb4b1356087b36c3c60621c3d183483af75
lovef <380772858@qq.com>
2018-05-24 02:32:27 +0800

    v0.15

--- common_static/js/init.js
+++ common_static/js/init.js
@@ -3,6 +3,8 @@
 
     $('.sidenav').sidenav();
     $('.tabs').tabs();
+    $('.dropdown-trigger').dropdown();
+    $('.tooltipped').tooltip();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
